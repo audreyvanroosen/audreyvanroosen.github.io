174a51adbf37b5ef84df4a76a683cacf98bf0c86
audreyvanroosen <audreyvanroosen@Audreys-MBP.home>
2017-01-15 23:25:09 -0500

    Homework RGB / I will be coming to office hours tomorrow

---
+++ class_work/class_07/first-javascript/script.js
@@ -0,0 +1,16 @@
+// alert('This is an alert. This is annoying');
+
+
+document.write(10 * 10);
+document.write(100 / 10);
+document.write(50 - 17);
+document.write(50 + 17);
+document.write('Audrey Van Roosen');
+
+var num1 = 10 * 10;
+document.write(num1);
+
+var myName = "Audrey Van Roosen";
+document.write(myName);
+
+document.getElementById('title').style.color = 'red';
